f5b409fe025cf82442d03beabefc0dc0ce989be7
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-03-07 12:16:29 -0800

    commit

--- test.py
+++ test.py
@@ -13,10 +13,12 @@ chat = tele.bot.get_chat(CREDENTIALS['channel'])
 
 def test(url):
 	result = reddit_2_album.get(url)
-	album_sender.send_v2(chat, result)
+	# album_sender.send_v2(chat, result)
+	print(result)
 	
 if __name__=='__main__':
 	# test('http://www.reddit.com/r/Feminism/comments/lz1nj2/truth/')
 	# test('http://www.reddit.com/r/Feminism/comments/lyz02f/behold_the_protective_power_of_soiled_sanity_pads/')
 	# test('http://www.reddit.com/r/Feminism/comments/lya1fw/the_answer_is_no/')
-	test('https://www.reddit.com/r/Feminism/comments/lwz97t/dump_the_dimorphism_between_female_and_male_brain/')
+	# test('https://www.reddit.com/r/Feminism/comments/lwz97t/dump_the_dimorphism_between_female_and_male_brain/')
+	test('https://www.reddit.com/r/Feminism/comments/lu9nbg/exactly/')
